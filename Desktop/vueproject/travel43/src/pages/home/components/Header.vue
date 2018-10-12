6ded7d541e9767608fcb98a25863ea9aaf79f04d
2016soft <2016soft@iMac43.local>
2018-10-12 11:53:14 +0800

    add Home Header

---
+++ Desktop/vueproject/travel43/src/pages/home/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="header">
+    <div class="header_left">
+        左
+    </div>
+    <div class="header_input">
+        中
+    </div>
+    <div class="header_right">
+        右
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped>
+.header{
+    height: .86rem;
+    line-height: .88rem;
+    background-color: gold;
+    display: flex;
+}
+.header_left{
+    float: left;
+    width: .6rem;
+}.header_input{
+    flex: 1;
+    background-color: #ffffff;
+    border-radius: 3px;
+    height: .64rem;
+    margin: .12rem;
+    line-height: .64rem;
+}.header_right{
+    float: right;
+}
+</style>
---
+++ Desktop/vueproject/travel43/src/pages/home/components/Swiper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div >
+    Swiper
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeSwiper'
+}
+</script>
+
+<style>
+</style>
